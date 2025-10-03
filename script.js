1f07d1190198ee87a9487695e5f5e0dc48bbc66c
cluis21165 <cluis21165@iesjoanramis.org>
2025-10-03 08:51:17 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,10 +9,28 @@ function canviFoto(){
 }
 
 function harryPotter(){
-    if(document.getElementById("foto").hidden == true){
-        document.getElementById("foto").hidden = false
-    }else{
-        document.getElementById("foto").hidden = true
-    }
+    document.getElementById("foto").style = "display:none"
    
+}
+
+function canviaFons(){
+    let body = document.getElementsByTagName("body")^[0]
+    let foto = prompt("¿DÓNDE QUIERES VIAJAR?")
+    body.style = "backround-image: url('"+foto+"'); backround-size: cover;";
+}
+
+function tradueix(){
+    let body = document.getElementsByTagName("body")^[0]
+    body.style = "font-family: 'Times New Roman', Times, serif;"
+}
+
+function afegirInfo(){
+    let llista = document.getElementById("expL");
+    let element = document.createElement("li")
+    element.innerHTML = prompt("¿QUÉ QUIERES AÑADIR")
+    llista.appendChild(element)
+}
+
+function color(){
+    
 }
